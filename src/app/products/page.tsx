3b53e10b594fe39b08b8f32714e3baf42e292d16
Shivam Deswal <129525543+shivamdeswal@users.noreply.github.com>
2025-05-13 12:44:46 +0530

    Client Side Data Fetching

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductList from './product-list'
+
+export default function page() {
+  return (
+    <div>
+      <h1>CLIENT SIDE DATA FETCHING ...</h1>
+      <ProductList />
+    </div>
+  )
+}
---
+++ src/app/products/product-list.tsx
@@ -0,0 +1,55 @@
+
+'use client';
+
+import { useEffect, useState } from 'react';
+import style from './style.module.css';
+
+type Product = {
+  id: number;
+  title: string;
+  thumbnail: string;
+  description: string;
+  price: number;
+};
+
+export default function ProductList() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('https://dummyjson.com/products?limit=10');
+        const data = await res.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return (
+      <h2 className={style.alignLoading}> Loading Products </h2>
+    );
+  }
+
+  if (!products) return <p>Failed to load products.</p>;
+
+  return (
+    <div className={style.productList}>
+      {products.map((product) => (
+        <div key={product.id} className={style.productCard}>
+          <h2>{product.title}</h2>
+          <img src={product.thumbnail} height='150px'  width='150px'/>
+          <p className={style.discription}>{product.description}</p>
+          <p><b>Price: ${product.price}</b></p>
+        </div>
+      ))}
+    </div>
+  );
+}
