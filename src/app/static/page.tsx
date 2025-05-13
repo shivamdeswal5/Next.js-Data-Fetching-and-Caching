c6646e8117a92679e95eba61be9449933068b584
Shivam Deswal <129525543+shivamdeswal@users.noreply.github.com>
2025-05-13 17:09:14 +0530

    Caching

---
+++ src/app/static/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default async function page() {
+    const res = await fetch('https://dummyjson.com/products/1');
+    const data = await res.json();
+  return (
+    <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>This is our static page</h1>
+      <p>
+        {
+            data.title
+        }
+      </p>
+      <img src={data.thumbnail} alt="" />
+      <p>{data.description}</p>
+    </div>
+  )
+}
