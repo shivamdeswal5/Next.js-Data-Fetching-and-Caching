b8c81396a237bbbeb51e038b5a862612b8ff59be
shivamdeswal5 <deswalworks@gmail.com>
2025-05-14 09:24:58 +0530

    Static Page

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default async function page() {
-    const res = await fetch('https://dummyjson.com/products/1');
+    const res = await fetch('https://dummyjson.com/products/1',{ cache: 'no-store' });
     const data = await res.json();
   return (
     <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
